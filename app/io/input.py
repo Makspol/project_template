be8fe04049598bb6e24afde7da863da66e6c58cd
Makspol <polishchuk.maks@gmail.com>
2024-03-16 19:05:45 +0200

    The functions for reading and writing are declared in the input.py and output.py files, respectively.

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def read_line_from_console(message):
+    pass
+
+
+def read_file(path):
+    pass
+
+
+def read_file_with_pandas(path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def write_to_console(text):
+    pass
+
+
+def write_file(file, file_name):
+    pass
+
+
+def write_file_with_pandas(file, file_name):
+    pass
